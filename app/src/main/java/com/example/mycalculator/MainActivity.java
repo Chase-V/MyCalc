793429d0c75500470fd8ec85a1b854a0bba4cead
Chase-V <38985553+Chase-V@users.noreply.github.com>
2021-08-05 19:34:34 +0300

    3ДЗ. Закончил. Калькулятор теперь все считает, состояния сохраняет. Выбрал примитивный способ сохранения состояний из-за того, что нужно сохранить небольшое количсетсво элементов, и такой вариант на мой взгляд значительно менее ресурсоемкий для этих целей. Готово к проверке.

--- app/src/main/java/com/example/mycalculator/MainActivity.java
+++ app/src/main/java/com/example/mycalculator/MainActivity.java
@@ -5,37 +5,59 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.text.DecimalFormat;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button button0;
-    Button button1;
-    Button button2;
-    Button button3;
-    Button button4;
-    Button button5;
-    Button button6;
-    Button button7;
-    Button button8;
-    Button button9;
-    Button buttonAC;
-    Button buttonDivide;
-    Button buttonMultiply;
-    Button buttonPercent;
-    Button buttonDelete;
-    Button buttonMinus;
-    Button buttonPlus;
-    Button buttonEquals;
-    Button buttonDot;
-    TextView currentNums;
-    TextView historyNums;
+    private static final char addition = '+';
+    private static final char subtraction = '-';
+    private static final char multiplication = '*';
+    private static final char division = '/';
+    private static final char module = '%';
+    private final String CurrentNumbersKey = "CurrentNumbers";
+    private final String HistoryNumbersKey = "HistoryNumbers";
+    private final String Val1Key = "Val1";
+    private final String Val2Key = "Val2";
+    private final String CurrentActionKey = "CurrentAction";
+    private Button button0;
+    private Button button1;
+    private Button button2;
+    private Button button3;
+    private Button button4;
+    private Button button5;
+    private Button button6;
+    private Button button7;
+    private Button button8;
+    private Button button9;
+    private Button buttonAC;
+    private Button buttonDivide;
+    private Button buttonMultiply;
+    private Button buttonPercent;
+    private Button buttonDelete;
+    private Button buttonMinus;
+    private Button buttonPlus;
+    private Button buttonEquals;
+    private Button buttonDot;
+    private Button buttonNegative;
+    private TextView currentNumbers;
+    private TextView historyNumbers;
+    private double valueOne = Double.NaN;
+    private double valueTwo;
+    private DecimalFormat decimalFormat;
+    private char currentAction;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        decimalFormat = new DecimalFormat("#.##########");
+
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
         initViews();
@@ -45,69 +67,144 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     @Override
-    public void onClick(View v) {
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
 
-        switch (v.getId()) {
-            case R.id.button0:
-                currentNums.append("0");
-                break;
-            case R.id.button1:
-                currentNums.append("1");
-                break;
-            case R.id.button2:
-                currentNums.append("2");
-                break;
-            case R.id.button3:
-                currentNums.append("3");
-                break;
-            case R.id.button4:
-                currentNums.append("4");
-                break;
-            case R.id.button5:
-                currentNums.append("5");
-                break;
-            case R.id.button6:
-                currentNums.append("6");
-                break;
-            case R.id.button7:
-                currentNums.append("7");
-                break;
-            case R.id.button8:
-                currentNums.append("8");
-                break;
-            case R.id.button9:
-                currentNums.append("9");
-                break;
-            case R.id.buttonDot:
-                if (currentNums.getText().toString().contains(".")) {
-                    break;
-                } else currentNums.append(".");
-                break;
-            case R.id.buttonAC:
-                currentNums.setText("");
-                historyNums.setText("");
-                break;
-            case R.id.buttonPlus:
-                historyNums.append(currentNums.getText()+"+");
-                currentNums.setText("");
-                break;
-            case R.id.buttonMinus:
-                historyNums.append(currentNums.getText()+"-");
-                currentNums.setText("");
-                break;
-            case R.id.buttonDelete:
-                if (currentNums.length()>0){
-                    currentNums.setText(currentNums.getText().toString().substring(0, currentNums.length()-1));
-                    break;
-                } else break;
-            default:
-                break;
-        }
-
+        outState.putString(CurrentNumbersKey, currentNumbers.getText().toString());
+        outState.putString(HistoryNumbersKey, historyNumbers.getText().toString());
+        outState.putDouble(Val1Key, valueOne);
+        outState.putDouble(Val2Key, valueTwo);
+        outState.putChar(CurrentActionKey, currentAction);
+        super.onSaveInstanceState(outState);
     }
 
     @Override
-    public void onPointerCaptureChanged(boolean hasCapture) {
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+
+        currentNumbers.setText(savedInstanceState.getString(CurrentNumbersKey));
+        historyNumbers.setText(savedInstanceState.getString(HistoryNumbersKey));
+        valueOne = savedInstanceState.getDouble(Val1Key);
+        valueTwo = savedInstanceState.getDouble(Val2Key);
+        currentAction = savedInstanceState.getChar(CurrentActionKey);
+        super.onRestoreInstanceState(savedInstanceState);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+
+            case R.id.button0:
+                currentNumbers.append("0");
+                break;
+
+            case R.id.button1:
+                currentNumbers.append("1");
+                break;
+
+            case R.id.button2:
+                currentNumbers.append("2");
+                break;
+
+            case R.id.button3:
+                currentNumbers.append("3");
+                break;
+
+            case R.id.button4:
+                currentNumbers.append("4");
+                break;
+
+            case R.id.button5:
+                currentNumbers.append("5");
+                break;
+
+            case R.id.button6:
+                currentNumbers.append("6");
+                break;
+
+            case R.id.button7:
+                currentNumbers.append("7");
+                break;
+
+            case R.id.button8:
+                currentNumbers.append("8");
+                break;
+
+            case R.id.button9:
+                currentNumbers.append("9");
+                break;
+
+            case R.id.buttonDot:
+                if (currentNumbers.getText().toString().contains(".")) {
+                    break;
+                } else currentNumbers.append(".");
+                break;
+
+            case R.id.buttonAC:
+                valueOne = Double.NaN;
+                valueTwo = Double.NaN;
+                currentNumbers.setText("");
+                historyNumbers.setText("");
+                break;
+
+            case R.id.buttonPlus:
+                computeCalculation();
+                currentAction = addition;
+                historyNumbers.setText(decimalFormat.format(valueOne) + "+");
+                currentNumbers.setText(null);
+                break;
+
+            case R.id.buttonMinus:
+                computeCalculation();
+                currentAction = subtraction;
+                historyNumbers.setText(decimalFormat.format(valueOne) + "-");
+                currentNumbers.setText(null);
+                break;
+
+            case R.id.buttonMultiply:
+                computeCalculation();
+                currentAction = multiplication;
+                historyNumbers.setText(decimalFormat.format(valueOne) + "*");
+                currentNumbers.setText(null);
+                break;
+
+            case R.id.buttonDivide:
+                computeCalculation();
+                currentAction = division;
+                historyNumbers.setText(decimalFormat.format(valueOne) + "/");
+                currentNumbers.setText(null);
+                break;
+
+            case R.id.buttonNegative:
+                if (currentNumbers.length() > 0) {
+                    currentNumbers.setText(decimalFormat.format(Double.parseDouble(currentNumbers.getText().toString()) * (-1)));
+                }
+                break;
+
+            case R.id.buttonPercent:
+                computeCalculation();
+                currentAction = module;
+                historyNumbers.setText(decimalFormat.format(valueOne) + "%");
+                currentNumbers.setText(null);
+                break;
+
+            case R.id.buttonEquals:
+                computeCalculation();
+                historyNumbers.setText(historyNumbers.getText().toString() +
+                        decimalFormat.format(valueTwo) + " = " + decimalFormat.format(valueOne));
+                currentNumbers.setText(decimalFormat.format(valueOne));
+                valueOne = Double.NaN;
+                currentAction = '0';
+                break;
+
+            case R.id.buttonDelete:
+                if (currentNumbers.length() > 0) {
+                    currentNumbers.setText(currentNumbers.getText().toString().substring(0, currentNumbers.length() - 1));
+                    break;
+                } else break;
+
+            default:
+                break;
+        }
 
     }
 
@@ -129,6 +226,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         buttonDelete.setOnClickListener(this);
         buttonMinus.setOnClickListener(this);
         buttonPlus.setOnClickListener(this);
+        buttonNegative.setOnClickListener(this);
         buttonEquals.setOnClickListener(this);
         buttonDot.setOnClickListener(this);
     }
@@ -153,7 +251,31 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         buttonPlus = findViewById(R.id.buttonPlus);
         buttonEquals = findViewById(R.id.buttonEquals);
         buttonDot = findViewById(R.id.buttonDot);
-        currentNums = findViewById(R.id.textViewCurrentNums);
-        historyNums = findViewById(R.id.textViewOperationHistory);
+        buttonNegative = findViewById(R.id.buttonNegative);
+        currentNumbers = findViewById(R.id.textViewCurrentNums);
+        historyNumbers = findViewById(R.id.textViewOperationHistory);
+    }
+
+    private void computeCalculation() {
+        if (!Double.isNaN(valueOne)) {
+            valueTwo = Double.parseDouble(currentNumbers.getText().toString());
+            currentNumbers.setText(null);
+
+            if (currentAction == addition)
+                valueOne = this.valueOne + valueTwo;
+            else if (currentAction == subtraction)
+                valueOne = this.valueOne - valueTwo;
+            else if (currentAction == multiplication)
+                valueOne = this.valueOne * valueTwo;
+            else if (currentAction == division)
+                valueOne = this.valueOne / valueTwo;
+            else if (currentAction == module)
+                valueOne = this.valueOne % valueTwo;
+        } else {
+            try {
+                valueOne = Double.parseDouble(currentNumbers.getText().toString());
+            } catch (Exception e) {
+            }
+        }
     }
 }
